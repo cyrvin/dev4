e2b50c4d833de1a750b35df1b601defada6ed98b
cyrvin <crllvnc@gmail.com>
2014-07-27 12:23:33 +0200

    first

--- web.js
+++ web.js
@@ -1,50 +1,103 @@
-// web.js
 var express = require("express");
 var logfmt 	= require("logfmt");
-//var pg 		= require('pg');
 var request = require('request');
 var cheerio = require('cheerio');
+var pg 		= require('pg');
+
+//var DATABASE_URL = 'postgres://xirltryzxcpdls:yRStNpzO4hmyAvJV5TOZ92mDqY@ec2-54-197-241-95.compute-1.amazonaws.com:5432/daqgo67bqvvbal';
+//var databaseUrl = process.env.DATABASE_URL || DATABASE_URL;
+
+var DATABASE_URL = 'postgresql://cyrillevincey@localhost/cyrillevincey'
+var databaseUrl = DATABASE_URL;
 
 var app = express();
-//var DATABASE_URL = 'postgres://xirltryzxcpdls:yRStNpzO4hmyAvJV5TOZ92mDqY@ec2-54-197-241-95.compute-1.amazonaws.com:5432/daqgo67bqvvbal';
 
 app.use(logfmt.requestLogger());
 
-app.use(express.static(__dirname + '/public'));
-
 app.get('/', function(req, res) {
-	res.render('index.html');
-});
-
-console.log("Registering endpoint: /connection");
-app.get('/connection/', function(req, res){
-	var publicUrl = req.query.puburl;
-
-	getSkillsFromPublicUrl(publicUrl, function(error, skills){
-		console.log(skills);
-		res.send(skills);
-	});
-
+	getActivitiesFromCompanyPage('http://fr.kompass.com/c/schunk-intec-sarl/fr8394169/');
 });
 
 var port = Number(process.env.PORT || 5000);
+
 app.listen(port, function() {
 	console.log("Listening on " + port);
 });
 
-function getSkillsFromPublicUrl(publicUrl, callback) {
+//console.log($(this).text().replace(/\s+/g, " ").trim()); //ok
+//console.log($(this).attr('href')); //ok
+//console.log($(this).text()); //ok
 
-	request(publicUrl, function(error, response, html) {
+function getActivitiesFromCompanyPage(companyUrl) {
 
-		if (error) 
-			callback(error);
-		else {
-			var $ = cheerio.load(html);
-			var skills = [];
-			$('#profile-skills .content .skills li .jellybean').each(function(i, skill) {
-				skills.push($(this).text().replace(/\s+/g, " ").trim());				
-			});
-			callback(error,skills);
-		}
+	request(companyUrl, function(error, response, html) {
+		var $ = cheerio.load(html);
+		scrapeActivities($, '#mainActivitiesTree > ul > li > a', 'primary', companyUrl);
+		scrapeActivities($, '#secondaryActivitiesTree > ul > li > a', 'secondary', companyUrl);
 	});
 };
+
+function scrapeActivities($, path, rank, companyUrl) {
+	$(path).each(function() {
+		var parentActivity = processActivity($(this), rank, null);
+		writeCompanyActivity(parentActivity, companyUrl);
+		$(this).parent().find('ul').find('li').find('a').each(function() {
+			var childActivity = processActivity($(this), rank, parentActivity.url);
+			writeCompanyActivity(childActivity, companyUrl);
+		});
+	});
+}
+
+function processActivity(dollarThis, rank, parentUrl) {
+	var activity = {};
+
+	activity.rank = rank;
+	activity.url = dollarThis.attr('href');
+	activity.parentUrl = parentUrl;
+
+	var label = dollarThis.text();
+
+	var roleAndLabel = parseRoleAndLabel(label);
+	activity.label = roleAndLabel.label;
+	activity.role = roleAndLabel.role;
+
+	return activity;
+}
+
+function parseRoleAndLabel(label) {
+	var roleAndLabel = {};
+
+	if (label.lastIndexOf('(P)') > 0) {
+		roleAndLabel.role = 'P';
+		roleAndLabel.label = label.substring(0, (label.lastIndexOf('(P)') - 1));
+	} else if (label.lastIndexOf('(D)') > 0) {
+		roleAndLabel.role = 'D';
+		roleAndLabel.label = label.substring(0, (label.lastIndexOf('(D)') - 1));
+	} else if (label.lastIndexOf('(S)') > 0) {
+		roleAndLabel.role = 'S';
+		roleAndLabel.label = label.substring(0, (label.lastIndexOf('(S)') - 1));
+	} else {
+		roleAndLabel.role = null;
+		roleAndLabel.label = label;
+	}
+
+	return roleAndLabel;
+}
+
+function writeCompanyActivity(activity, companyUrl) {
+	console.log('company : ', companyUrl);
+
+	pg.connect(databaseUrl, function(err, client, done) {
+		client.query(
+			'INSERT into CompanyActivities (companyUrl, rank, activityUrl, label, role, parentUrl) VALUES($1, $2, $3, $4, $5, $6)', 
+			[companyUrl, activity.rank, activity.url, activity.label, activity.role, activity.parentUrl], 
+			function(err, result) {
+				if (err) {
+					console.log(err);
+				} else {
+					console.log('row inserted for ' + companyUrl + ' ' + activity.url);
+				}
+				client.end();
+			});        
+	});
+}
---
+++ web_old2.js
@@ -0,0 +1,51 @@
+// web.js
+var express = require("express");
+var logfmt 	= require("logfmt");
+var request = require('request');
+var cheerio = require('cheerio');
+
+var app = express();
+
+//var pg 		= require('pg');
+//var DATABASE_URL = 'postgres://xirltryzxcpdls:yRStNpzO4hmyAvJV5TOZ92mDqY@ec2-54-197-241-95.compute-1.amazonaws.com:5432/daqgo67bqvvbal';
+
+app.use(logfmt.requestLogger());
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+	res.render('index.html');
+});
+
+console.log("Registering endpoint: /connection");
+app.get('/connection/', function(req, res){
+	var publicUrl = req.query.puburl;
+
+	getSkillsFromPublicUrl(publicUrl, function(error, skills){
+		console.log(skills);
+		res.send(skills);
+	});
+
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+	console.log("Listening on " + port);
+});
+
+function getSkillsFromPublicUrl(publicUrl, callback) {
+
+	request(publicUrl, function(error, response, html) {
+
+		if (error) 
+			callback(error);
+		else {
+			var $ = cheerio.load(html);
+			var skills = [];
+			$('#profile-skills .content .skills li .jellybean').each(function(i, skill) {
+				skills.push($(this).text().replace(/\s+/g, " ").trim());				
+			});
+			callback(error,skills);
+		}
+	});
+};
